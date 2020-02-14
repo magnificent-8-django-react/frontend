62bcaa6ce763a50899c8c6e52eaaf57768282c9b
geekwise-ayanna <geekwise.ayanna.bs@gmail.com>
2020-02-14 11:16:13 -0800

    added: login page save for a couple buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,56 @@
-import React, { Component } from "react";
-
+import React from "react";
 import Login from "./components/accounts/Login";
-import Register from "./components/accounts/Register";
-import Header from "./components/layout/Header";
-import Home from "./components/Home";
-
-// import PrivateRoute from "./components/common/PrivateRoute";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-import { Provider } from 'react-redux';
-import store from "./store";
-import { loadUser } from './actions/auth';
 
-class App extends Component {
-  componentDidMount() {
-    store.dispatch(loadUser());
-  }
+function App() {
+  return (<Router>
+    <div className="App">
+        <div className="container">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+              </li>
+            </ul>
+          </div>
+        </div>
 
-  render() {
-    return (
-      <Provider store={store}>
-          <Router>
-            <Header/>
-            <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route exact path="/login" component={Login}/>
-              <Route path="/register" component={Register}/>
-            </Switch>
-          </Router>
-      </Provider>
-    );
-  }
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
 }
 
 export default App;
+
+
+// class App extends Component {
+//   componentDidMount() {
+//     store.dispatch(loadUser());
+//   }
+
+//   render() {
+//     return (
+//       <Provider store={store}>
+//           <Router>
+//             <Header/>
+//             <Switch>
+//               <Route exact path="/" component={Home}/>
+//               <Route exact path="/login" component={Login}/>
+//               <Route path="/register" component={Register}/>
+//             </Switch>
+//           </Router>
+//       </Provider>
+//     );
+//   }
+// }
+
+// export default App;
--- frontend/src/components/accounts/Login.js
+++ frontend/src/components/accounts/Login.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import logo from "../../images/food-truck-logo.png"
 
 export default class Login extends Component {
     render() {
         return (
             <div>
-                <h1>Login Form</h1>
+            
+            <img src={logo} alt="Dine Drive Logo" className="logo"/>
+
+            <h1 className="welcome">Welcome, Sign in to continue</h1>
+
+            <form>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Username" />
+                </div>
+
+                <div className="form-group">
+                    <input type="password" className="form-control" placeholder="Password" />
+                </div>
+
+                <p className="forgot-password text-right">
+                    Forgot password?
+                </p>
+
+                <button type="submit" className="btn btn-primary btn-block">Get Started</button>
+
+            </form>
             </div>
-        )
+        );
     }
 }
