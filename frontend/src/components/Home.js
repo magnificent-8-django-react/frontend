1d87eeab6ce52d29d6f75b2883d4cefc97735847
snowhero <xionghiroyuki@gmail.com>
2020-02-18 09:48:13 -0800

    udate: working demo truck user location update to database

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -27,13 +27,15 @@ export class Home extends Component {
         // mylat: 36.7378,
         // mylng: -119.7871,
         userid: "",
-        truckid: "",
         mylat: 0,
         mylng: 0,
+        myTruck: {},
         zoom: 10,
         location_permission: true,
         labels: [],
-        locations: []
+        locations: [],
+
+        isUser: false
     }
 
     componentDidMount() {
@@ -50,25 +52,56 @@ export class Home extends Component {
           .get('http://127.0.0.1:8000/restaurant/profiles/')
           .then( res => {
               this.setState({ locations: res.data.results });
+              
               // If truck user, <Test if owners in restaurantProfiles have the same id as user>
             //   this.setState({ userid: this.props.auth.user.id });
-              
+              this.setState({ userid: 100 });
               // True --> <Do an axios.put to update the corresponding truck location with mylat/mylng states above>
               // False --> <Continue, no need to update user location because they do not have lat/long fields>   
               // GRABS OWNER ID AND MATCH WITH CURRENT USER TO SEE IF USER OWNS A TRUCK
               //  console.log(parseInt(this.state.locations[0].owner.substring(28).slice(0, -1))); --> OWNER ID
-            //   for(let i=0; i<this.state.locations.length; i++) {
-            //     if(this.state.userid === parseInt(this.state.locations[i].owner.substring(28).slice(0, -1))) {
-            //         this.setState({ truckid: this.state.userid});
-            //     }
-            //     break;
-            //   }
-
-              console.log(this.state.locations);
+              for(let i=0; i<this.state.locations.length; i++) {
+                if(this.state.userid === parseInt(this.state.locations[i].owner.substring(28).slice(0, -1))) {
+                    console.log('ran');
+                    this.setState({ myTruck: {
+                        "restaurant": this.state.locations[i].restaurant,
+                        "username": this.state.locations[i].username,
+                        "email": this.state.locations[i].email,
+                        "phone": this.state.locations[i].phone,
+                        "lat": this.state.mylat,
+                        "long": this.state.mylng
+                    }})
+                    const config = {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }
+                    axios
+                      .put(`http://127.0.0.1:8000/restaurant/profiles/${this.state.locations[i].id}/`, this.state.myTruck, config)
+                      .then(res => {
+                        console.log('successful update');
+                        axios
+                          .get('http://127.0.0.1:8000/restaurant/profiles/')
+                          .then( res => {
+                              this.setState({ locations: res.data.results });
+                              console.log(this.state.locations);
+                          })
+                          .catch( err => {
+                              console.log(err);
+                          })  
+                      })
+                      .catch(err => {
+                        console.log('unsuccessful update');
+                      });
+                    break;
+                }
+                this.setState({ isUser: true });
+              };
           })
           .catch( err => {
               console.log(err)
           })
+
     };
 
     componentWillUnmount() {
@@ -120,7 +153,7 @@ export class Home extends Component {
                     {this.state.locations.map( location => (
                         this.renderTrucks(location)
                     ))}
-                    {this.state.location_permission ?
+                    {this.state.location_permission && this.state.isUser ?
                         <Marker
                             key={3} 
                             position={[lat, lng]}
